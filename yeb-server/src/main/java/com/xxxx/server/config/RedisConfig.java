f80e252118828abf5d8d653bbac1ab003883dc9f
junw <jun.wu@nuaa.edu.cn>
2021-05-20 14:46:23 +0800

    1. 配置redis 2.将用户菜单放置在redis中

--- yeb-server/src/main/java/com/xxxx/server/config/RedisConfig.java
+++ yeb-server/src/main/java/com/xxxx/server/config/RedisConfig.java
@@ -3,9 +3,11 @@ package com.xxxx.server.config;
 import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import sun.rmi.transport.proxy.RMIDirectSocketFactory;
 
 /**
  * Redis配置类
@@ -16,7 +18,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String,Object> redisTemplate(){
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
         RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
         //String类型 key序列器
         redisTemplate.setKeySerializer(new StringRedisSerializer());
@@ -27,6 +29,7 @@ public class RedisConfig {
 
         redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
     }
 }
--- yeb-server/src/main/java/com/xxxx/server/service/impl/MenuServiceImpl.java
+++ yeb-server/src/main/java/com/xxxx/server/service/impl/MenuServiceImpl.java
@@ -1,14 +1,19 @@
 package com.xxxx.server.service.impl;
 
+import com.alibaba.excel.util.CollectionUtils;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xxxx.server.mapper.MenuMapper;
 import com.xxxx.server.pojo.Admin;
 import com.xxxx.server.pojo.Menu;
 import com.xxxx.server.service.IMenuService;
+import org.ehcache.core.util.CollectionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -25,12 +30,22 @@ public class MenuServiceImpl extends ServiceImpl<MenuMapper, Menu> implements IM
 
     @Autowired
     private MenuMapper menuMapper;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
     /**
      * 通过用户id去查询菜单
      * @return
      */
     @Override
     public List<Menu> getMenusByAdminId() {
-        return menuMapper.getMenusByAdminId(((Admin)(SecurityContextHolder.getContext().getAuthentication().getPrincipal())).getId());
+        Integer adminId = ((Admin)(SecurityContextHolder.getContext().getAuthentication().getPrincipal())).getId();
+        ValueOperations<String,Object> valueOperations = redisTemplate.opsForValue();
+        List<Menu> menus =(List<Menu>) valueOperations.get("menu_" + adminId);
+        if(CollectionUtils.isEmpty(menus)){
+            menus = menuMapper.getMenusByAdminId(adminId);
+            valueOperations.set("menu_" + adminId,menus);
+        }
+        return menus;
     }
 }
